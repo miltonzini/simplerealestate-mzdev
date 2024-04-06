289060665bf57136795e8d575bd1ea1e90793182
miltonzini <miltonzini@gmail.com>
2024-04-06 13:11:27 -0300

    Update property view

--- resources/views/property.blade.php
+++ resources/views/property.blade.php
@@ -1,10 +1,52 @@
 <x-layout>
-    <h1>Property</h1>
+    <h1>Propiedad {{ $property->id }}</h1>
     <main>
-        <section>
-            <div class="container">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia repellat, a expedita ratione sint nobis quos ex veritatis exercitationem. Aut omnis vel deserunt odit. Quas recusandae ullam ab exercitationem aliquid.</p>
+            <div class="dark-table" role="region" tabindex="0">
+                <table>
+                <thead>
+                    <tr>
+                        <th colspan="2">Detalles</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Título</td>
+                        <td>{{ $property->title }}</td>
+                    </tr>
+                    <tr>
+                        <td>Descripción</td>
+                        <td>{{ $property->description }}</td>
+                    </tr>
+                    <tr>
+                        <td>Dirección</td>
+                        <td>{{ $property->address }}</td>
+                    </tr>
+                    <tr>
+                        <td>Ciudad</td>
+                        <td>{{ $property->city }}</td>
+                    </tr>
+                    <tr>
+                        <td>Precio</td>
+                        <td>{{ $property->price }}</td>
+                    </tr>
+                    <tr>
+                        <td>Tipo</td>
+                        <td>{{ $property->type }}</td>
+                    </tr>
+                    <tr>
+                        <td>Estado</td>
+                        <td>{{ $property->status }}</td>
+                    </tr>
+                    <tr>
+                        <td>Fecha de creación</td>
+                        <td>{{ $property->created_at }}</td>
+                    </tr>
+                    <tr>
+                        <td>Fecha de actualización</td>
+                        <td>{{ $property->updated_at }}</td>
+                    </tr>
+                </tbody>
+                        </table>
             </div>
-        </section>
     </main>
 </x-layout>
