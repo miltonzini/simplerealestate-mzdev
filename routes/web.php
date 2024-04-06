e2be1ca67c0395255b0ebbca40995b06fa68d6c5
miltonzini <miltonzini@gmail.com>
2024-04-06 12:46:58 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/property', function () {
+    return view('property');
+})->name('property');
+
+Route::get('/properties', function () {
+    return view('properties');
+})->name('properties');
