5e4e770c32c5ba4e0f4ed86024bddd1a8a536be2
miltonzini <miltonzini@gmail.com>
2024-04-06 12:44:12 -0300

    Create several views
    create views and add boilerplate content

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <h1>About</h1>
+    <main>
+        <section>
+            <div class="container">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia repellat, a expedita ratione sint nobis quos ex veritatis exercitationem. Aut omnis vel deserunt odit. Quas recusandae ullam ab exercitationem aliquid.</p>
+            </div>
+        </section>
+    </main>
+</x-layout>
