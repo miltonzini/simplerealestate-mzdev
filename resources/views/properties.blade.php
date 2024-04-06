2dc4ac9df2b869d50fc7e525579ed91e5df4f864
miltonzini <miltonzini@gmail.com>
2024-04-06 13:10:08 -0300

    Update properties view and styles

--- resources/views/properties.blade.php
+++ resources/views/properties.blade.php
@@ -1,10 +1,42 @@
 <x-layout>
     <h1>Properties</h1>
     <main>
-        <section>
-            <div class="container">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia repellat, a expedita ratione sint nobis quos ex veritatis exercitationem. Aut omnis vel deserunt odit. Quas recusandae ullam ab exercitationem aliquid.</p>
-            </div>
-        </section>
+        <div class="dark-table" role="region" tabindex="0">
+            <table>
+                <caption>Listado de Propiedades</caption>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>title</th>
+                        <th>description</th>
+                        <th>address</th>
+                        <th>city</th>
+                        <th>price</th>
+                        <th>type</th>
+                        <th>status</th>
+                        <th>created_at</th>
+                        <th>updated_at</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($properties as $property)
+                    <tr>
+                        <td>{{ $property->id }}</td>
+                        <td>{{ $property->title }}</td>
+                        <td>{{ $property->description }}</td>
+                        <td>{{ $property->address }}</td>
+                        <td>{{ $property->city }}</td>
+                        <td>{{ $property->price }}</td>
+                        <td>{{ $property->type }}</td>
+                        <td>{{ $property->status }}</td>
+                        <td>{{ $property->created_at }}</td>
+                        <td>{{ $property->updated_at }}</td>
+                        <td><a href="{{ env('BASE_URL') }}/properties/{{ $property->id }}">Ver detalles</a></td>
+
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </main>
 </x-layout>
