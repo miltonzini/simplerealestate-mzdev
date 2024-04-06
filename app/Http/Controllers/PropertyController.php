c625c3fd5e9c5708710b087d8953977110aad5f1
miltonzini <miltonzini@gmail.com>
2024-04-06 12:57:30 -0300

    Update PropertyController and route

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Property;
 
 class PropertyController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $property = Property::find($id);
+
+        if (!$property) {
+            abort(404);
+        }
+
+        return view('property', ['property' => $property]);
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PropertyController;
 
 Route::get('/', function () {
     return view('home');
@@ -14,10 +15,8 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/property', function () {
-    return view('property');
-})->name('property');
-
 Route::get('/properties', function () {
     return view('properties');
 })->name('properties');
+
+Route::get('/properties/{id}', [PropertyController::class, 'show'])->name('property');
