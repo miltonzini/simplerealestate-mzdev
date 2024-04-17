536086294449b8efa637d7dcecd02b259f943e05
miltonzini <miltonzini@gmail.com>
2024-04-17 13:47:43 -0300

    update home template and styles
    add basic navigation anchors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,19 @@
     <main>
         <section>
             <div class="container">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia repellat, a expedita ratione sint nobis quos ex veritatis exercitationem. Aut omnis vel deserunt odit. Quas recusandae ullam ab exercitationem aliquid.</p>
+                <h3>Links internos:</p>
+                <ul>
+                    <li><a href="{{ route('home') }}" class="button button-sm">Home</a></li>
+                    <li><a href="{{ route('about') }}" class="button button-sm">About</a></li>
+                    <li><a href="{{ route('properties') }}" class="button button-sm">Properties</a></li>
+                    <li><a href="{{ route('contact') }}" class="button button-sm">Contact</a></li>
+                </ul>
+
+                <h3>Rutas de prueba:</p>
+                <ul>
+                    <li><a href="{{ route('test-view') }}" class="button button-sm">Test view: "/test-view"</a></li>
+                    <li><a href="{{ route('test-string') }}" class="button button-sm">Test String: "/test-string/{string}"</a></li>
+                </ul>
             </div>
         </section>
     </main>
