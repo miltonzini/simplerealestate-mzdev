a043d8b5e633b493942bbfca3a89c5d27acd7865
miltonzini <miltonzini@gmail.com>
2024-04-18 11:24:43 -0300

    update navbar
    use setActiveRoute helper function

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,10 +9,10 @@
 <body>
         <nav class="navbar">
             <div class="first-block ">
-                <a href="{{ route('home') }}" class="button navbar-link {{ request()->routeIs('home') ? 'active' : '' }}">Home</a>
-                <a href="{{ route('about') }}" class="button navbar-link {{ request()->routeIs('about') ? 'active' : '' }}">About</a>
-                <a href="{{ route('properties') }}" class="button navbar-link {{ request()->routeIs('properties') ? 'active' : '' }}">Properties</a>
-                <a href="{{ route('contact') }}" class="button navbar-link {{ request()->routeIs('contact') ? 'active' : '' }}">Contact</a>
+                <a href="{{ route('home') }}" class="button navbar-link {{ setActiveRoute('home') }}">Home</a>
+                <a href="{{ route('about') }}" class="button navbar-link {{ setActiveRoute('about') }}">About</a>
+                <a href="{{ route('properties') }}" class="button navbar-link {{ setActiveRoute('properties') }}">Properties</a>
+                <a href="{{ route('contact') }}" class="button navbar-link {{ setActiveRoute('contact') }}">Contact</a>
             </div>
             <div class="second-block">
                 <!-- <a href="{{ route('home') }}" class="navbar-link {{ request()->routeIs('home') ? 'active' : '' }}">Home</a> -->
