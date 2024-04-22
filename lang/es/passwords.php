689b50e7c151a3573bb5679bbf36d81a8aed1891
miltonzini <miltonzini@gmail.com>
2024-04-22 10:45:55 -0300

    update lang/es messages

--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Hemos enviado por correo electrónico el enlace para restablecer tu contraseña.',
+    'throttled' => 'Por favor, espera antes de intentarlo de nuevo.',
+    'token' => 'Este token para restablecer la contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
 
 ];
